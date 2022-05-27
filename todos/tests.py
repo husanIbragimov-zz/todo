82ae8c6ba67ecea5cb149e66ecae7222cd3e3e12
Xusan <husanIbragimov>
2022-05-28 01:07:52 +0500

    api

---
+++ todos/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .models import Todo
+
+
+class TodoModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(title='Backend', body='Django framework')
+
+    def test_title_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.title}'
+        self.assertEqual(expected_object_name, 'Backend')
+
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEqual(expected_object_name, 'Django framework')
+
