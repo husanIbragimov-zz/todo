f8efe62910d32075a64396dbf18b1b24b6337312
Xusan <husanIbragimov>
2022-05-28 01:15:07 +0500

    created_at

--- todos/models.py
+++ todos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
--- todos/serializes.py
+++ todos/serializes.py
@@ -5,4 +5,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'body',)
+        fields = ('id', 'title', 'body', 'created_at')
